f544ff271a16b04c5c866506e810fe01c962e3bd
Yuanyuan Wang <38868191+YYW-UMN@users.noreply.github.com>
2020-02-18 15:18:34 -0600

    Update and rename findSuspectsKNN.R to findSuspectsKNN.r

--- findSuspectsKNN.r
+++ findSuspectsKNN.r
@@ -12,11 +12,11 @@ findSuspectsKNN <- function(
   n_snps <- ncol(x)
   
   # normalize, scale and run pca, tsne and KNN to find suspects  
-  r <- as.matrix(assays(x)$count / assays(x)$depth)
-  r[is.nan(r)] <- 0
-  
-  #r <- scale(log(10000 * assays(x)$count / Matrix::rowSums(assays(x)$depth) + 1)) # second scaling method
-  #r <- scale(Normalize(assays(x)$count/(assays(x)$depth+0.01))) #third scaling method
+  r <- scale(log(10000 * assays(x)$count / Matrix::rowSums(assays(x)$depth) + 1)) 
+  #r <- as.matrix(assays(x)$count / assays(x)$depth)             
+  #r[is.nan(r)] <- 0
+  #r <- scale(r)
+  #r <- scale(Normalize(assays(x)$count/(assays(x)$depth+0.01)))
   
   u <- irlba(r, nu = n_pc, nv = 1, work = 20, maxit = 2000)$u # u = sample ~ n_pc
   knn <- knn.index(u, k = knn_k) 
@@ -27,7 +27,7 @@ findSuspectsKNN <- function(
   )
   # count pseudo neighbors for each row of input data
   n_pseudo_neighbor <- Matrix::rowSums(KNN[, rowData(x)$is_pseudo])
-  is_suspect <- n_pseudo_neighbor > knn_threshold & !rowData(x)$is_pseudo # more neighbors were pseudo than real data
+  is_suspect <- n_pseudo_neighbor > knn_threshold & !rowData(x)$is_pseudo 
   if (n_samples < 100) {
     y <- Rtsne(u, check_duplicates = FALSE, perplexity = 20)$Y
   } else {
